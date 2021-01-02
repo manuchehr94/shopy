5ecd10ec1826a7bbd3f44ab9f16d699726b0f3e4
manuchehr94 <manuchehr.muhidinov@gmail.com>
2021-01-02 11:39:52 +0500

    Add files via upload

---
+++ shop/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+	
+	$('.menuToggle').on('click', function(){
+		
+		$('.menu').slideToggle(300, function(){
+			if($(this).css('display') === "none") {
+				$(this).removeAttr('style');
+				//$(this).addAttr('z-index: 999');
+			}
+		});
+		
+	});
+});
